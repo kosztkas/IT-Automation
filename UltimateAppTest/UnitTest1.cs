ce6627b2e76801644c3d4033840cdc2c7391ba1f
Sanyi <kosztkas@sch.bme.hu>
2022-04-29 11:13:34 +0200

    Create tests

--- UltimateAppTest/UnitTest1.cs
+++ UltimateAppTest/UnitTest1.cs
@@ -16,5 +16,33 @@ namespace UltimateApp.Test
             int? retVal = Calculator.calculate(2, 3, Operation.Add);
             Assert.AreEqual(5, retVal);
         }
+
+        [TestMethod]
+        public void SubtractTest()
+        {
+            int? retVal = Calculator.calculate(3, 3, Operation.Subtract);
+            Assert.AreEqual(0, retVal);
+        }
+
+        [TestMethod]
+        public void MultiplyTest()
+        {
+            int? retVal = Calculator.calculate(2, 3, Operation.Multiply);
+            Assert.AreEqual(6, retVal);
+        }
+
+        [TestMethod]
+        public void DivideTest()
+        {
+            int? retVal = Calculator.calculate(6, 3, Operation.Divide);
+            Assert.AreEqual(2, retVal);
+        }
+
+        [TestMethod]
+        public void ModuloTest()
+        {
+            int? retVal = Calculator.calculate(10, 5, Operation.Modulo);
+            Assert.AreEqual(0, retVal);
+        }
     }
 }
